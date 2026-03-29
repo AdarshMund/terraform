7ac4d8d0fbf0c466d6acda8d2104599e3cce3128
AdarshMund <adarshkumarmund755@gmail.com>
2026-03-30 03:26:01 +0530

    Delete backend.tf

--- backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "my-terraform-state-bktt1122"  
-    key            = "rds/terraform.tfstate"      
-    region         = "ap-south-1"                 
-    dynamodb_table = "terraform-lock-table"       
-    encrypt        = true
-  }
-}
